2cb0c62cdce0e129236633d2e294e31195b0671f
dachachou <113423009+dachachou@users.noreply.github.com>
2026-04-01 21:24:55 -0400

    Update API base URL to use environment variable

--- src/api.js
+++ src/api.js
@@ -7,10 +7,19 @@
 
 
 
+//import axios from 'axios';
+
+//const api = axios.create({
+//  baseURL: 'http://localhost:4000/api' // apunta al backend en puerto 4000
+//});
+
+//export default api;
+
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api' // apunta al backend en puerto 4000
+  baseURL: process.env.REACT_APP_API_URL // ahora toma la URL del backend en Render
 });
 
 export default api;
